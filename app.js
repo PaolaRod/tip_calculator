351863422d2bb60810d7f37fffee208b7c45fcb6
Paola <pao.alejandra1992@gmail.com>
2021-08-26 17:21:33 -0300

    remove border input

--- app.js
+++ app.js
@@ -1,67 +1,61 @@
-const billBtn = document.querySelector('.container__bill input');
-const peopleBtn = document.querySelector('.container__people input');
-const inputTipAmount = document.querySelector('[data-tip-amount]');
-const inputTotal = document.querySelector('[data-amount-total]');
+const billBtn = document.querySelector(".container__bill input");
+const peopleBtn = document.querySelector(".container__people input");
+const inputTipAmount = document.querySelector("[data-tip-amount]");
+const inputTotal = document.querySelector("[data-amount-total]");
 
 const tipApp = () => {
-    let tipAmount = 0;
-    let total = 0;
+  let tipAmount = 0;
+  let total = 0;
 
-    const selectTip = () => {
-        const tips = document.querySelectorAll('.tip');
-        const zero = document.querySelector('.zero');
+  const selectTip = () => {
+    const tips = document.querySelectorAll(".tip");
+    const zero = document.querySelector(".zero");
 
-        billBtn.addEventListener('click', () => {
-            billBtn.classList.add('focusCyan');
-        });
+    billBtn.addEventListener("click", () => {
+      billBtn.classList.add("focusCyan");
+    });
 
-        peopleBtn.addEventListener('click', () => {
-            peopleBtn.classList.add('focusRed');
-        });
+    peopleBtn.addEventListener("click", () => {
+      peopleBtn.classList.add("focusRed");
+    });
 
-        tips.forEach(tip => {
-            tip.addEventListener('click', () => {
-                tipAmount = parseInt(billBtn.value) * (parseInt(tip.innerText) / 100);
+    tips.forEach((tip) => {
+      tip.addEventListener("click", () => {
+        if (parseInt(peopleBtn.value) === 0 || peopleBtn.value === "") {
+          console.log("No puede ser 0");
+          zero.classList.add("fadeIn");
+          return;
+        } else if (zero.classList.contains("fadeIn")) {
+          zero.classList.remove("fadeIn");
+          zero.classList.add("fadeOut");
+        } 
+        tipAmount = parseInt(billBtn.value) * (parseInt(tip.innerText) / 100);
+        inputTotal.textContent = tipAmount.toFixed(2);
+        total = tipAmount / parseInt(peopleBtn.value);
+        inputTipAmount.textContent = total.toFixed(2);
+        billBtn.classList.remove("focusCyan");
+        peopleBtn.classList.remove("focusRed");
+      });
+    });
+  };
 
-                if (parseInt(peopleBtn.value) === 0 || peopleBtn.value === '') {
-                    console.log('No puede ser 0');
-                    zero.classList.add('fadeIn');
-                } else if (zero.classList.contains('fadeIn')) {
-                    zero.classList.remove('fadeIn');
-                    zero.classList.add('fadeOut');
-                } else {
-                    inputTotal.textContent = tipAmount.toFixed(2);
-                    total = tipAmount / parseInt(peopleBtn.value);
-                    inputTipAmount.textContent = total.toFixed(2);
-                }
-            })
-        })
-    }
+  const resetForm = () => {
+    const resetBtn = document.querySelector(".reset");
 
+    resetBtn.addEventListener("click", () => {
+      billBtn.value = "";
+      peopleBtn.value = "";
+      inputTotal.textContent = "0.00";
+      inputTipAmount.textContent = "0.00";
+      billBtn.classList.remove("focusCyan");
+      peopleBtn.classList.remove("focusRed");
+      // tipAmount = 0;
+      // total = 0;
+    });
+  };
 
-    const resetForm = () => {
-        const resetBtn = document.querySelector('.reset');
-
-        resetBtn.addEventListener('click', () => {
-            billBtn.value = "";
-            peopleBtn.value = "";
-            inputTotal.textContent = '0.00';
-            inputTipAmount.textContent = '0.00';
-            billBtn.classList.remove('focusCyan');
-            peopleBtn.classList.remove('focusRed');
-            // tipAmount = 0;
-            // total = 0;
-        });
-
-    }
-
-    selectTip();
-    resetForm();
-
-}
+  selectTip();
+  resetForm();
+};
 
 tipApp();
-
-
-
-
